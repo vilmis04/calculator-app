61441b94684a81f8dc2334b7cbddc58b59003b9f
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-03-08 21:12:17 +0200

    upd: color standards added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Display from './components/Display/Display';
 import InputKeys from './components/InputKeys/InputKeys';
 import ThemeSelector from './components/ThemeSelector/ThemeSelector';
+import { darkMode, lightMode, contrastMode } from './colorSchemes';
 
 function App() {
+
+  const [ colorScheme, setColorTheme ] = useState(darkMode);
+
   return (
     <div id='app'>
       <div id='header'>
         <div id='logo'>calc</div>
-        <ThemeSelector />
+        <ThemeSelector theme={colorScheme} />
       </div>
       <Display />
       <InputKeys />
---
+++ src/colorSchemes.tsx
@@ -0,0 +1,48 @@
+export const darkMode = {
+    mainBackground: 'hsl(222, 26%, 31%)',
+    keypadBackground: 'hsl(223, 31%, 20%)',
+    screenBackground: 'hsl(224, 36%, 15%)',
+
+    del_reset_background: 'hsl(225, 21%, 49%)',
+    del_reset_shaddow: 'hsl(224, 28%, 35%)',
+    equal_toggle_background: 'hsl(6, 63%, 50%)',
+    equal_toggle_shaddow: 'hsl(6, 70%, 34%)',
+    keysBackground: 'hsl(30, 25%, 89%)',
+    keysShaddow: 'hsl(28, 16%, 65%)',
+
+    primaryText: 'hsl(221, 14%, 31%)',
+    secondaryText: 'hsl(0, 0, 100%)'
+  }
+
+export const lightMode = {
+    mainBackground: 'hsl(0, 0%, 90%)',
+    keypadBackground: 'hsl(0, 5%, 81%)',
+    screenBackground: 'hsl(0, 0%, 93%)',
+
+    del_reset_background: 'hsl(185, 42%, 37%)',
+    del_reset_shaddow: 'hsl(185, 58%, 25%)',
+    equal_toggle_background: 'hsl(25, 98%, 40%)',
+    equal_toggle_shaddow: 'hsl(25, 99%, 27%)',
+    keysBackground: 'hsl(45, 7%, 89%)',
+    keysShaddow: 'hsl(35, 11%, 61%)',
+
+    primaryText: 'hsl(60, 10%, 19%)',
+    secondaryText: 'hsl(0, 0, 100%)'
+  }
+
+export const contrastMode = {
+    mainBackground: 'hsl(268, 75%, 9%)',
+    keypadBackground: 'hsl(268, 71%, 12%)',
+    screenBackground: 'hsl(268, 71%, 12%)',
+
+    del_reset_background: 'hsl(281, 89%, 26%)',
+    del_reset_shaddow: 'hsl(285, 91%, 52%)',
+    equal_toggle_background: 'hsl(176, 100%, 44%)',
+    equal_toggle_shaddow: 'hsl(177, 92%, 70%)',
+    keysBackground: 'hsl(268, 47%, 21%)',
+    keysShaddow: 'hsl(290, 70%, 36%)',
+
+    primaryText: 'hsl(52, 100%, 62%)',
+    secondaryText: 'hsl(198, 20%, 13%)',
+    contrastOnlyText: 'hsl(0, 0, 100%)'
+  }
