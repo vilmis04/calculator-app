ad19047629deec36f416fde6e6abcd1bf9f3c628
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-03-08 19:55:27 +0200

    upd: add elements to ThemeSelector

--- src/components/ThemeSelector/ThemeSelector.tsx
+++ src/components/ThemeSelector/ThemeSelector.tsx
@@ -3,5 +3,48 @@
 // }
 
 export default function ThemeSelector (props: any) {
-    return null;
+    
+
+
+    const buttonStyle = {
+        width: '1rem',
+        height: '1rem',
+        borderRadius: '50%',
+        border: 'none',
+        backgroundColor: 'red',
+    }
+
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '3.5rem'
+    }
+
+    const mainContainerStyle = {
+        display: 'flex'
+    }
+
+    const themeLabelStyle = {
+        alignSelf: 'end',
+        marginRight: '2rem'
+    }
+
+    return (
+        <div style={mainContainerStyle} >
+            <div style={themeLabelStyle}>THEME</div>
+            <div>
+                <div style={containerStyle} >
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
+                <div style={containerStyle} >
+                    <button style={buttonStyle} ></button>
+                    <button style={buttonStyle} ></button>
+                    <button style={buttonStyle} ></button>
+                </div>
+            </div>
+        </div>
+    );
 }
